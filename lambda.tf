23f8a7e396a488fb141de633d66b7722cac5ae56
andrews <novadina@hotmail.com>
2025-12-05 05:52:49 +0700

    upload files

---
+++ lambda.tf
@@ -0,0 +1,90 @@
+# Package the Lambda function code
+data "archive_file" "lambda_invoke_dynamodb" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/invoke-dynamodb/app.py"
+  output_path = "${path.module}/lambda/invoke-dynamodb/function.zip"
+}
+
+data "archive_file" "lambda_invoke_s3" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/invoke-s3/app.py"
+  output_path = "${path.module}/lambda/invoke-s3/function.zip"
+}
+
+data "archive_file" "lambda_invoke_glue" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/invoke-glue/app.py"
+  output_path = "${path.module}/lambda/invoke-glue/function.zip"
+}
+
+# Lambda function
+resource "aws_lambda_function" "invoke_dynamodb" {
+  filename         = data.archive_file.lambda_invoke_dynamodb.output_path
+  function_name    = "${lambda_funnction_name_prefix}_dynamodb"
+  role             = aws_iam_role.lambda_role_dynamodb.arn
+  handler          = "app.lambda_handler"
+  source_code_hash = data.archive_file.lambda_invoke_dynamodb.output_base64sha256
+
+  runtime = "python3.13"
+
+  environment {
+    variables = {
+      ENVIRONMENT = "production"
+      LOG_LEVEL   = "info"
+      TABLE_NAME  = var.table_name
+    }
+  }
+
+  tags = {
+    Environment = "production"
+    Application = "App1"
+  }
+}
+
+
+resource "aws_lambda_function" "invoke_s3" {
+  filename         = data.archive_file.lambda_invoke_s3.output_path
+  function_name    = "${lambda_funnction_name_prefix}_s3"
+  role             = aws_iam_role.lambda_role_s3.arn
+  handler          = "app.lambda_handler"
+  source_code_hash = data.archive_file.lambda_invoke_s3.output_base64sha256
+
+  runtime = "python3.13"
+
+  environment {
+    variables = {
+      ENVIRONMENT = "production"
+      LOG_LEVEL   = "info"
+      BUCKET_NAME = var.bucket_name
+    }
+  }
+
+  tags = {
+    Environment = "production"
+    Application = "App2"
+  }
+}
+
+
+resource "aws_lambda_function" "invoke_glue" {
+  filename         = data.archive_file.lambda_invoke_glue.output_path
+  function_name    = "${lambda_funnction_name_prefix}_glue"
+  role             = aws_iam_role.lambda_role_glue.arn
+  handler          = "app.lambda_handler"
+  source_code_hash = data.archive_file.lambda_invoke_glue.output_base64sha256
+
+  runtime = "python3.13"
+
+  environment {
+    variables = {
+      ENVIRONMENT = "production"
+      LOG_LEVEL   = "info"
+      GLUE_JOB_NAME = var.job_name
+    }
+  }
+
+  tags = {
+    Environment = "production"
+    Application = "App3"
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,53 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region to deploy into"
+  default     = "ca-central-1"
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "Enter Your Project Bucket Name"
+  default     = "as-metroc-terraform-2025-tfstate"
+}
+
+variable "glue_bucket_name" {
+  type        = string
+  description = "Enter Your Glue Bucket Name"
+  default     = "as-metroc-glue-2025"
+}
+
+variable "lambda_iam_role_prefix" {
+  type        = string
+  description = "Enter Your IAM Role Prefix"
+  default     = "Lambda_Execution_Role_with_Invoke"
+}
+
+variable "glue_iam_role_prefix" {
+  type        = string
+  description = "Enter Your IAM Role Prefix"
+  default     = "Glue_Execution_Role_with_Invoke"
+}
+
+variable "lambda_funnction_name_prefix" {
+  type        = string
+  description = "Enter Your IAM Role Prefix"
+  default     = "lambda_function_invoke"
+}
+
+variable "api_name" {
+  type        = string
+  description = "Enter Your API Name"
+  default     = "AWS_Serverless_Multi_Channel_API"
+}
+
+variable "table_name" {
+  type        = string
+  description = "Enter Your DynamoDB table name"
+  default     = "lambda-apigateway"
+}
+
+variable "job_name" {
+  type        = string
+  description = "Enter Your Glue Job name"
+  default     = "app3-etl-job"
+}
